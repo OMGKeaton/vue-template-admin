9ebcb513289d32cd7d5f5cced6c0e88be10b34e9
admin <844864184@qq.com>
2022-09-26 09:34:44 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+import * as API from '@/api/product/index'
+import CategorySelector from '@/components/CategorySelector'
+import HintButton from '@/components/HintButton'
+// 全局注册组件
+Vue.component('CategorySelector',CategorySelector)
+Vue.component('HintButton',HintButton)
+
+// API就是下面这个对象
+// {
+//   trademark:{},
+  // attr:{}
+  // spu:{}
+  // sku:{}
+// }
+
+// this.$API.trademark.delete
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+Vue.prototype.$API = API  //API是包含了attr spu sku trademark所有的接口请求函数对象
+
+ 
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
---
+++ src/views/product/Spu/SkuForm/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-form label-width="100px">
+    <el-form-item label="SPU名称"> spu名称 </el-form-item>
+
+    <el-form-item label="SKU名称">
+      <el-input v-model="model" placeholder="SKU名称"></el-input>
+    </el-form-item>
+
+    <el-form-item label="价格(元)">
+      <el-input v-model="model" placeholder="价格(元)" type="number"></el-input>
+    </el-form-item>
+
+    <el-form-item label="重量(千克)">
+      <el-input
+        v-model="model"
+        placeholder="重量(千克)"
+        type="number"
+      ></el-input>
+    </el-form-item>
+
+    <el-form-item label="规格描述">
+      <el-input
+        v-model="model"
+        placeholder="规格描述"
+        type="textarea"
+        rows="4"
+      ></el-input>
+    </el-form-item>
+
+    <el-form-item label="平台属性">
+      <el-form :inline="true" label-width="100px">
+        <el-form-item label="产地">
+          <el-select value="" placeholder="请选择">
+            <el-option label="label" value="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-form-item>
+
+    <el-form-item label="销售属性">
+      <el-form :inline="true" label-width="100px">
+        <el-form-item label="颜色">
+          <el-select value="" placeholder="请选择">
+            <el-option label="label" value="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-form-item>
+
+    <el-form-item label="图片列表">
+      <el-table border style="width: 100%">
+        <el-table-column align="center" type="selection" width="55"> </el-table-column>
+        <el-table-column prop="prop" label="图片" width="width">
+        </el-table-column>
+        <el-table-column prop="prop" label="名称" width="width">
+        </el-table-column>
+        <el-table-column prop="prop" label="操作" width="width">
+        </el-table-column>
+      </el-table>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary">保存</el-button>
+      <el-button @click="$emit('update:isShowSkuForm',false)">取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: "SkuForm",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
